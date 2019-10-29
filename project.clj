ff1b133d82ac342ef6b6aab2397c87e8ff9be73f
Adrian Vasile Rosian <adrian.rosian@gmail.com>
2019-10-29 10:35:46 +0200

    Cors attempt

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
  (defproject tzonner "0.1.0-SNAPSHOT"
    :description "TimeZonnes CRUD"
    :dependencies [[org.clojure/clojure "1.10.0"]
-                  [metosin/compojure-api "2.0.0-alpha30"]]
+                  [metosin/compojure-api "2.0.0-alpha30"]
+                  [ring-cors "0.1.13"]]
    :ring {:handler tzonner.handler/app}
    :uberjar-name "server.jar"
    :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
--- src/tzonner/handler.clj
+++ src/tzonner/handler.clj
@@ -1,7 +1,8 @@
 (ns tzonner.handler
   (:require [compojure.api.sweet :refer :all]
             [ring.util.http-response :refer :all]
-            [clojure.spec.alpha :as s]))
+            [clojure.spec.alpha :as s]
+            [ring.middleware.cors :refer [wrap-cors]]))
 
 (def state (atom []))
 
@@ -10,11 +11,16 @@
 (s/def ::city
   #(contains? #{"Alba Iulia" "Cluj Napoca" "Munich" "London"} %))
 (s/def ::offset int?)
+(s/def ::dst boolean?)
 (s/def ::timezone (s/keys
-  :req-un [::name ::city ::offset] :opt-un [::id]))
+  :req-un [::name ::city ::offset ::dst] :opt-un [::id]))
 
 (s/def ::error string?)
-(s/def ::error-map (s/keys :req [::error]))
+(s/def ::error-map (s/keys :req-un [::error]))
+
+(defn cors-md [hnd]
+  (wrap-cors hnd :access-control-allow-origin [#"http://localhost:3449"]
+    :access-control-allow-methods [:options :get :put :post :delete]))
 
 (def app
   (api
@@ -31,11 +37,13 @@
 
       (GET "/timezones" []
         :return (s/coll-of ::timezone :into []) 
+        :middleware [cors-md]
         :summary "List of timezones"
         (ok @state))
 
       (GET "/timezones/:id" []
         :path-params [id :- ::id]
+        :middleware [cors-md]
         :return (s/or :tz ::timezone :err ::error-map) 
         :summary "Get in the timeZone"
         (if-let [found (->> @state
@@ -43,10 +51,47 @@
           first)]
           (ok found)
           (not-found {:error "No such timezone"})))
+      
+      (PUT "/timezones/:id" []
+        :path-params [id :- ::id]
+        :body [dstobj (s/keys :req-un [::dst])]
+        :return (s/or :tz ::timezone :err ::error-map) 
+        :middleware [cors-md]
+        :summary "Change DST status"
+        (if-let [found (->> @state
+            (filterv #(= id (:id %)))
+            first)]
+          (let [modified (some->
+              found
+              (assoc :dst (-> dstobj :dst)))]
+            (reset!
+              state
+              (map 
+                #(if (= (:id %) id) modified %)
+                @state))
+            (println @state modified)
+            (ok modified))
+          (not-found {:error "No such timezone"})))
+      
+      (DELETE "/timezones/:id" []
+        :path-params [id :- ::id]
+        :return (s/or :tz ::timezone :err ::error-map) 
+        :middleware [cors-md]
+        :summary "Delete a timezone"
+        (if-let [found (->> @state
+            (filterv #(= id (:id %)))
+            first)]
+          (do
+            (reset!
+              state
+              (filter #(not= id (:id %)) @state))
+            (ok found))
+          (not-found {:error "No such timezone"})))
 
       (POST "/timezones" []
         :return ::timezone
         :body [tz ::timezone]
+        :middleware [cors-md]
         :summary "Create a timezone"
         (let [id (java.util.UUID/randomUUID)
           marked (assoc tz :id id)]
